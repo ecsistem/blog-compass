75bc61c42b7216295af9781c5a5eb15959085d71
Edson Costa <edson.13.10.2004@gmail.com>
2023-07-25 18:29:48 -0300

    :sparkles: feat: Area de Noticias

--- src/components/NewsArea/newsarea.ts
+++ src/components/NewsArea/newsarea.ts
@@ -1,15 +1,59 @@
 import "./newsarea.css";
+
+interface NewsData {
+  id: number;
+  title: string;
+  image: string;
+  content: string;
+}
+
+const newsData: NewsData[] = [
+  {
+    id: 1,
+    title: "The Future",
+    image: "https://github.com/ecsistem.png",
+    content: "Lorem Ipsum is simply dummy text...",
+  },
+  {
+    id: 2,
+    title: "The Future",
+    image: "https://github.com/ecsistem.png",
+    content: "Lorem Ipsum is simply dummy text...",
+  },
+  {
+    id: 3,
+    title: "The Future",
+    image: "https://github.com/ecsistem.png",
+    content: "Lorem Ipsum is simply dummy text...",
+  },
+  {
+    id: 4,
+    title: "The Future",
+    image: "https://github.com/ecsistem.png",
+    content: "Lorem Ipsum is simply dummy text...",
+  },
+  {
+    id: 5,
+    title: "The Future",
+    image: "https://github.com/ecsistem.png",
+    content: "Lorem Ipsum is simply dummy text...",
+  },
+  {
+    id: 6,
+    title: "The Future",
+    image: "https://github.com/ecsistem.png",
+    content: "Lorem Ipsum is simply dummy text...",
+  },
+  // Adicione mais notícias aqui
+];
+
 export function createNewsArea(): HTMLElement {
   const NewsAreaContent = `
     <article class="posts">
-        <h1 class="title">User posts</h1>
-        <div class="card-post">
-        <img src="https://github.com/ecsistem.png" alt="title do post">
-    <div class="container-card">
-        <h3 class="card-title">The Future</h3>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took...</p>
-        <a id="Expand">Expand...</a>
-        </div>
+      <h1 class="article-title">User posts</h1>
+      <div class="cards-posts">
+        ${generateNewsCards()}
+      </div>
     </article>
   `;
 
@@ -17,4 +61,21 @@ export function createNewsArea(): HTMLElement {
   newsarea.innerHTML = NewsAreaContent.trim();
 
   return newsarea;
+}
+
+function generateNewsCards(): string {
+  return newsData
+    .map(
+      (news) => `
+        <div class="card-post">
+          <img class="article-image" src=${news.image} alt=${news.title}>
+          <div class="container-card">
+            <h3 class="card-title">${news.title}</h3>
+            <p>${news.content}</p>
+            <a class="expand" href="noticia.html?id=${news.id}&title=${news.title}">Expand...</a>
+          </div>
+        </div>
+      `
+    )
+    .join("");
 }
