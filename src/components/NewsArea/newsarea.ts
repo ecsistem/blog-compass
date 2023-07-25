cbe4e4558211e018b5a8f0debf8918d150a5bcc8
Edson Costa <edson.13.10.2004@gmail.com>
2023-07-25 18:28:01 -0300

    :sparkles: feat: Pagina de Detalhe de Post

--- src/components/NewsArea/newsarea.ts
+++ src/components/NewsArea/newsarea.ts
@@ -3,6 +3,13 @@ export function createNewsArea(): HTMLElement {
   const NewsAreaContent = `
     <article class="posts">
         <h1 class="title">User posts</h1>
+        <div class="card-post">
+        <img src="https://github.com/ecsistem.png" alt="title do post">
+    <div class="container-card">
+        <h3 class="card-title">The Future</h3>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took...</p>
+        <a id="Expand">Expand...</a>
+        </div>
     </article>
   `;
 
