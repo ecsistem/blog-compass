de862acc077e0d8d16d951a5a9a77eec05fab3d1
Edson Costa <edson.13.10.2004@gmail.com>
2023-07-27 20:43:48 -0300

    :sparkles: feat: Enviar para o link o card

--- src/components/NewsArea/newsarea.ts
+++ src/components/NewsArea/newsarea.ts
@@ -14,22 +14,35 @@ export function createNewsArea(): HTMLElement {
   const newsarea = document.createElement("main");
   newsarea.innerHTML = NewsAreaContent.trim();
 
+
+   const cardPosts = newsarea.querySelectorAll(".card-post");
+   cardPosts.forEach((cardPost) => {
+     cardPost.addEventListener("click", () => {
+      console.log("click");
+       // Obter os atributos data-id e data-title do card-post clicado
+       const id = cardPost.getAttribute("data-id");
+       const title = cardPost.getAttribute("data-title");
+        // Redirecionar para a página news.htmlk com os parâmetros id e title
+       window.location.href = `news.html?id=${id}&title=${title}`;
+     });
+   });
+ 
+
   return newsarea;
 }
 
 function generateNewsCards(): string {
+  
   return posts
     .map(
       (post) => `
-        <div class="card-post">
-        <a href="noticia.html?id=${post.id}&title=${post.title}">
+        <div class="card-post" data-id="${post.id}" data-title="${encodeURIComponent(post.title)}">
           <img class="article-image" src=${post.imageUrl} alt=${post.title}>
           <div class="container-card">
             <h3 class="card-title">${post.title}</h3>
             <p>${post.body}</p>
-            <a class="expand" href="noticia.html?id=${post.id}&title=${post.title}">Expand...</a>
+            <a class="expand" href="news.html?id=${post.id}&title=${encodeURIComponent(post.title)}">Expand...</a>
           </div>
-          </a>
         </div>
       `
     )
